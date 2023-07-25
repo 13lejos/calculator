7157beb41e14d79471add553864a59cd000e1d36
Joseph Le <13lejoseph@gmail.com>
2023-07-25 14:42:24 -0700

    define var and test onclick function

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-subtract = (a, b) => {
-let resultSubtract = a - b;
+// number to be stored as display
+let displayNumber = 0;
+// number to be operated on
+let operandNumber;
+// number to operate
+let operatorNumber;
+// current operator
+let operator;
+//stored number/answer
+let storedNumber;
+
+
+
+
+operate = (operandNumber, operatorNumber) => {
+storedNumber = operandNumber + operator + operatorNumber;
+}
+
+numberPress = (pressedNumber) => {
+alert(1)
 }
