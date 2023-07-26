899ba9a8c235f5901d5d3ee37147e21cdfd2ea51
Joseph Le <13lejoseph@gmail.com>
2023-07-25 17:34:48 -0700

    tweak operate function

--- script.js
+++ script.js
@@ -6,27 +6,39 @@ let operandNumber;
 let operatorNumber;
 // current operator
 let operator;
-//stored number/answer
+//stored number
 let storedNumber;
 //last button pressed stored
 let pressed;
+//final answer
+let finalAnswer;
 
+// when the window loads displays our default value 0 in our calc window
 window.onload = () => {
     document.getElementById(display);
     display.textContent= displayNumber;
 }
 
+// function to do math
+operate = (operator, storedNumber, displayNumber) => {
+if
+            storedNumber + Number(displayNumber);
+            storedNumber - Number(displayNumber);
+            storedNumber * Number(displayNumber);
+            storedNumber / Number(displayNumber);
+    
 
-operate = (operandNumber, operatorNumber) => {
-storedNumber = operandNumber + operator + operatorNumber;
 }
 
+//takes a number and adds it to the display
+//will clear if display is currently at 0
+//stops accepting numbers after 10 digits reached
 numberPress = (value) => {
     pressed = value.toString();
 if (displayNumber  < 1){
     displayNumber = pressed.toString();
 }
-else{
+else if(displayNumber.length <10) {
     displayNumber = displayNumber.toString() + pressed.toString();
 }
 
@@ -34,12 +46,27 @@ else{
     console.log(displayNumber);
     document.getElementById(display);
     display.textContent= displayNumber;
+    
 
 }
-
+// AC button clears
 allClear = () => {
     storedNumber = undefined;
+    operandNumber = undefined;
+    operatorNumber = undefined;
+    storedNumber = undefined;
+    pressed = undefined;
+    operator = undefined;
     displayNumber = 0;
     document.getElementById(display);
     display.textContent= displayNumber;
+}
+
+pressOperator = (value) =>{
+    storedNumber = Number(displayNumber);
+    displayNumber = Number(0);
+    operator = value;
+    console.log(storedNumber);
+    console.log(displayNumber);
+    console.log(operator);
 }
